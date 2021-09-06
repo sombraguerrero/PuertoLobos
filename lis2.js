44ca6fde801166a2a98926f4db62d6e57b2f7f93
Robert Setter <r.setter@comcast.net>
2021-09-06 15:24:52 -0500

    aligned randomization with the other webhooks

--- lis2.js
+++ lis2.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const FormData = require('form-data');
 const MersenneTwister = require('mersennetwister');
 const myConsts = require('./myConstants.js');
-function Artwork(num) {
+function Artwork() {
 	var basePath = "/var/services/homes/bobertdos/Google_Drive/WD_lis-media/";
-	if (Math.floor(num * 10) % 2 == 1) {
+	if (Math.round(MersenneTwister.random() * Number.MAX_SAFE_INTEGER) % 2 == 1) {
 		basePath += "/le-louvre-de-lis2/"
 	}
 	fs.readdir(basePath, { withFileTypes: true }, (err, files) => {
@@ -15,14 +15,13 @@ function Artwork(num) {
 			.filter(dirent => dirent.isFile())
 			.map(dirent => dirent.name);
 			//filteredFiles.sort();
-			var selectedImg = filteredFiles[Math.floor(num * filteredFiles.length)];
+			var selectedImg = filteredFiles[Math.round(MersenneTwister.random() * Number.MAX_SAFE_INTEGER) % filteredFiles.length];
 			//Perform post to Discord
 			var formData = new FormData();
 			formData.append('content', 'Random fan art: ' + selectedImg);
 			formData.append('file', fs.createReadStream(basePath + selectedImg), { filename: selectedImg});
 			formData.submit(`https://discordapp.com/api/webhooks/${myConsts.PL}`, (err, res) => {
-					var myLog = fs.createWriteStream('log.txt', { flags: 'w'});
-					myLog.write("[" + new Date().toLocaleString() + "]\r\nSelected Image: " + basePath + selectedImg + "\r\nResponse code: " + res.statusCode + "\r\nErrors: " + err + "\r\n");
+					console.log("[" + new Date().toLocaleString() + "]\r\nSelected Image: " + basePath + selectedImg + "\r\nResponse code: " + res.statusCode + "\r\nErrors: " + err + "\r\n");
 			});
 			//Question: Does the submit function fully dispose the object?
 			//Answer: Yes
@@ -39,5 +38,4 @@ function Artwork(num) {
 }
 
 // Navigate to and retrieve random file.
-var val = MersenneTwister.random();
-Artwork(val);
+Artwork();
